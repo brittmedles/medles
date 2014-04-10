2b71b6f92ed799cf1e386db1635184af96b48cae
Fara Britt Woolf <fbwoolf@gmail.com>
2014-04-10 11:00:24 -0500

    Login working again

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   
   // $("#signup_button1").on("click", function(event){
-//     event.preventDefault();
-//     $("#signup_form").show();
-//     $("#signup_button2").show();
-//     $("#tagline").hide();
-//     $(".separator").hide();
-//     $("#signup_button1").hide();
-//   });
+  //   event.preventDefault();
+  //   $("#signup").show();
+  //   $("#signup_form").show();
+  //   $("#signup_button2").show();
+  //   $("#tagline").hide();
+  //   $(".separator").hide();
+  //   $("#signup_button1").hide();
+  // });
   
   // $("#signup_button2").on("click", function(){
 //     $("#signup_form").hide();

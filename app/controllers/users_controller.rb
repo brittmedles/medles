c8542677a8770ede20f790d2c682cc7e105297fc
Fara Britt Woolf <fbwoolf@gmail.com>
2014-04-14 11:43:03 -0500

    Deleted unused views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,19 +34,20 @@ class UsersController < ApplicationController
   end
 
   def create
-     @user = User.new(params[:user])
-    
-     if @user.save
-       session[:user_id] = @user.id
-      
-       redirect_to(user_url(current_user.id))
-     else
-       render "new"
-     end
+   @user = User.new(params[:user])
+
+   if @user.save
+     session[:user_id] = @user.id
+  
+     redirect_to(user_url(current_user.id))
+   else
+     render "new"
+   end
   end
   
   def show
     @user = User.find(session[:user_id])
+    
     if params[:id].to_s == session[:user_id].to_s
     else
       redirect_to profile_url params[:id].to_i
